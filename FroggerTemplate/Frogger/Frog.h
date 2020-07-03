026d28e538b191244b527f86e1e4b2e5df773d29
Anna <annadanneker96@gmail.com>
2020-07-03 21:53:19 +0200

    aktuell

--- FroggerTemplate/Frogger/Frog.h
+++ FroggerTemplate/Frogger/Frog.h
@@ -1,10 +1,26 @@
 #ifndef FROG_H
 #define FROG_H
 
+class Terminal;
+
 class Frog
 {
 public:
-    
+    Frog();
+    void draw(Terminal& term);
+    void move_frog(char key);
+
+private:
+    enum class MoveDirection
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    };
+
+    //std::vector<Vec2D> _segments;
+    MoveDirection _dir;
 };
 
 #endif
